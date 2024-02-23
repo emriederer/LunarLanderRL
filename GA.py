ac0a67af16fa90527d02db1023cb1f2fab9340ff
SamuelReyes <100429033@alumnos.uc3m.es>
2024-02-23 16:02:07 +0100

    GA working

--- GA.py
+++ GA.py
@@ -11,41 +11,83 @@ env = gym.make("LunarLander-v2")
 
 class Genetic_Algorithm():
 
-    def __init__(self, N=100, crossover_method=1, pcross=1/80, pmut=0.1, sigma=0.1, selection=1, n_tour=4, n_iter=5, num_exp=1, elitism=1, bounds=[-20, 20]):
+    def __init__(self, N=100, pcross=0.6, pmut=0.1, sigma=0.1, n_tour=4, n_iter=200, num_exp=20, bounds=[-1, 1]):
         self.MLP = MLP([8, 6, 4])
+        self.chromosome_len = len(self.MLP.to_chromosome())
         self.N = N  # Tamaño de la población
+        self.bounds = bounds  # Límites del espacio de búsqueda
         self.pop = self.create_pop()  # Población
-        # Método de cruce [1: un punto, 2: uniforme]
-        self.crossover_method = crossover_method
         self.pcross = pcross  # Probabilidad de cruce
         self.pmut = pmut  # Probabilidad de mutación
-        self.sigma = sigma  # Desviación estándar de la mutación
-        # Método de selección de padres [1: torneo, 2: Boltzmann]
-        self.selection = selection
+        self.sigma = sigma  # Desviación estándar de la mutación gaussiana
         self.n_tour = n_tour  # Número de participantes en el torneo
         self.n_iter = n_iter  # Numero de iteraciones
-        # Número de individuos que pasan directamente a la siguiente generación
-        self.elitism = elitism
         self.num_exp = num_exp  # Número de experimentos por individuo
-        self.bounds = bounds
+
+    #############################
+    #######     INIT      #######
+    #############################
 
     def create_pop(self):
         """ Crea una población de N individuos codificados en modo locus """
-        chromosome_len = len(self.MLP.to_chromosome())
-        pop = [[random.uniform(-math.pi, math.pi)
-                for _ in range(chromosome_len)] for _ in range(self.N)]
+
+        pop = [[random.uniform(self.bounds[0], self.bounds[1])
+                for _ in range(self.chromosome_len)] for _ in range(self.N)]
         return pop
 
+    #############################
+    ######    MUTATION     ######
+    #############################
+
     def mutate(self, chromosome):
-        """ Mutación gausiana """
-        for i in range(len(chromosome)):
+        """ Mutación de un cromosoma """
+        for i, gen in enumerate(chromosome):
             if random.random() < self.pmut:
-                chromosome[i] += random.gauss(0, self.sigma)
+                mutations = [self.mutate_gaussian]
+
+                chromosome[i] = random.choice(mutations)(gen)
+
         return chromosome
+
+    def mutate_multiply(self, gen):
+        """ Mutación multiplicativa """
+        #! DO NOT USE
+        return gen * random.uniform(0.7, 1.5)
+
+    def mutate_gaussian(self, gen):
+        """ Mutación gaussiana """
+        return gen + random.gauss(0, self.sigma)
+
+    def mutate_sign(self, gen):
+        """ Mutación de signo """
+        #! DO NOT USE
+        return -gen
+
+    def mutate_init(self, gen):
+        """ Mutación de inicialización """
+        #! DO NOT USE
+        return random.uniform(self.bounds[0], self.bounds[1])
+
+    #############################
+    ######    CROSSOVER    ######
+    #############################
+
+    def crossover_one_point(self, parent1, parent2):
+        """Realiza un crossover entre dos padres."""
+
+        if random.random() < self.pcross:
+            crossover_point = random.randint(1, self.chromosome_len - 1)
+            child1 = parent1[:crossover_point] + parent2[crossover_point:]
+            child2 = parent2[:crossover_point] + parent1[crossover_point:]
+        else:
+            child1, child2 = parent1, parent2
+
+        return child1, child2
 
     def crossover_uniform(self, parent1, parent2):
         """Realiza un crossover uniforme entre dos padres."""
-        for i in range(len(parent1)):
+        #! DO NOT USE
+        for i in range(self.chromosome_len):
             beta = random.random()
             if random.random() < self.pcross:
                 parent1[i], parent2[i] = beta * parent1[i] + \
@@ -54,58 +96,15 @@ class Genetic_Algorithm():
 
         return parent1, parent2
 
-    def crossover_one_point(self, parent1, parent2):
-        """Realiza un crossover de un punto entre dos padres."""
-        if random.random() < self.pcross:
-            crossover_point = random.randint(1, len(parent1) - 1)
-            child1 = parent1[:crossover_point] + parent2[crossover_point:]
-            child2 = parent2[:crossover_point] + parent1[crossover_point:]
-        else:
-            child1, child2 = parent1, parent2
-
-        return child1, child2
-
     def crossover(self, parent1, parent2):
-        if self.crossover_method == 1:
-            return self.crossover_one_point(parent1, parent2)
-        elif self.crossover_method == 2:
-            return self.crossover_uniform(parent1, parent2)
-        else:
-            raise ValueError("Método de cruce no válido")
+        """Realiza un crossover entre dos padres."""
 
-    def tournament(self, fitness):
-        """ Selección de padres por torneo """
-        participants = random.sample(self.pop, self.n_tour)
-        best = None
-        for participant in participants:
-            if best is None:
-                best = participant
-            elif (fitness[self.pop.index(participant)] < fitness[self.pop.index(best)]):
-                best = participant
-        return best
+        return random.choice([self.crossover_one_point])(
+            parent1, parent2)
 
-    def select(self, fitness):
-        if self.selection == 1:
-            return self.tournament(fitness)
-        else:
-            raise ValueError("Método de selección no válido")
-
-    def create_children(self, fitness):
-        children = []
-        while len(children) < self.N:
-            parent1 = self.select(fitness)
-            parent2 = parent1
-            while parent1 == parent2:
-                parent2 = self.select(fitness)
-            child1, child2 = self.crossover_uniform(parent1, parent2)
-
-            self.mutate(child1)
-            self.mutate(child2)
-
-            children.append(child1)
-            children.append(child2)
-
-        return children
+    #############################
+    #####   LUNAR LANDER    #####
+    #############################
 
     def policy(self, observation):
         s = self.MLP.forward(observation)
@@ -127,6 +126,10 @@ class Genetic_Algorithm():
                 r = (racum+200) / 500
                 return racum
 
+    #############################
+    ######    FITNESS    ######
+    #############################
+
     def fitness(self, chromosome):
         """ Evalua un cromosoma """
         self.MLP.from_chromosome(chromosome)
@@ -136,6 +139,43 @@ class Genetic_Algorithm():
             r += self.run()
 
         return r/self.num_exp
+
+    #############################
+    ######    SELECTION    ######
+    #############################
+
+    def select(self, fitness):
+        """ Selección de padres por torneo """
+        participants = random.sample(range(self.N), self.n_tour)
+        best = None
+        for participant in participants:
+            if best is None:
+                best = participant
+            elif (fitness[participant] > fitness[best]):
+                best = participant
+        return self.pop[best]
+
+    def create_children(self, fitness):
+        children = []
+        while len(children) < self.N:
+            parent1 = self.select(fitness)
+            parent2 = self.select(fitness)
+            while parent1 == parent2:
+                parent2 = self.select(fitness)
+
+            child1, child2 = self.crossover(parent1, parent2)
+
+            self.mutate(child1)
+            self.mutate(child2)
+
+            children.append(child1)
+            children.append(child2)
+
+        return children
+
+    #############################
+    #######     PLOTS     #######
+    #############################
 
     def plot_evolution(self, fitness_history):
         plt.plot(fitness_history['best'], label='Best Fitness')
@@ -154,10 +194,31 @@ class Genetic_Algorithm():
             file.write(
                 f"Mejor fitness: {best_fitness}, Cromosoma: {best_chromosome}\n")
 
+    def log_params(self, file_path="evolution_log.txt"):
+        with open(file_path, "a") as file:
+            file.write(f"---------------------------------------------------\n")
+            file.write(f"Parámetros:\n")
+            file.write(f"N: {self.N}\n")
+            file.write(f"pcross: {self.pcross}\n")
+            file.write(f"pmut: {self.pmut}\n")
+            file.write(f"sigma: {self.sigma}\n")
+            file.write(f"n_tour: {self.n_tour}\n")
+            file.write(f"n_iter: {self.n_iter}\n")
+            file.write(f"num_exp: {self.num_exp}\n")
+            file.write(f"bounds: {self.bounds}\n")
+            file.write("\n")
+
+    #############################
+    ######    ALGORITHM    ######
+    #############################
+
     def evolve(self):
         """Evoluciona la población usando algoritmo genético."""
 
+        self.log_params(file_path="evolution_log.txt")
+
         fitness_list = [self.fitness(chromosome) for chromosome in self.pop]
+
         fitness_history = {'best': [max(fitness_list)], 'mean': [
             np.mean(fitness_list)]}
 
@@ -166,36 +227,15 @@ class Genetic_Algorithm():
 
         for generation in range(self.n_iter):
 
-            # Ordenar los individuos de la generación anterior
-            sorted_indices = np.argsort(fitness_list)[::-1]
-
             # Crear nueva generación y calcular su fitness
             children = self.create_children(fitness_list)
             # fitness_children = [self.fitness(chromosome) for chromosome in children]
             with concurrent.futures.ProcessPoolExecutor() as executor:
                 fitness_children = list(executor.map(self.fitness, children))
 
-            # Elitismo: seleccionar los N mejores individuos
-            if self.elitism:
-                new_pop = [self.pop[i] for i in sorted_indices[:self.elitism]]
-                fitness_list = [fitness_list[i]
-                                for i in sorted_indices[:self.elitism]]
-
-                new_pop = new_pop + \
-                    [children[i] for i in np.argsort(fitness_children)[
-                        ::-1][:self.N - self.elitism]]
-                fitness_list = fitness_list + \
-                    [fitness_children[i] for i in np.argsort(
-                        fitness_children)[::-1][:self.N - self.elitism]]
-
-            else:
-                new_pop = [children[i] for i in np.argsort(
-                    fitness_children)[::-1][:self.N - self.elitism]]
-                fitness_list = [fitness_children[i] for i in np.argsort(
-                    fitness_children)[::-1][:self.N - self.elitism]]
-
             # Actualizar la población
-            self.pop = new_pop
+            self.pop = children
+            fitness_list = fitness_children
 
             # Registra el mejor fitness y el fitness promedio
             fitness_history['best'].append(max(fitness_list))
